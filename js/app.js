e1bf4155f9f30a4deded302bfaf97cb8cb7a2eb3
Rodrigo <gil00013@algonquinlive.com>
2021-05-06 10:43:12 -0400

    Added materialize search-bar and modal

--- js/app.js
+++ js/app.js
@@ -2,12 +2,16 @@
 const APP = {
     init: () => {
         console.log('App Initialized');
-
         APP.addListeners();
     },
     addListeners: () => {
+        console.log('initializing add listeners')
         let elem = document.querySelectorAll('.sidenav')
-        M.Sidenav.init(elem)
+        M.Sidenav.init(elem, { dismissable: true });
+        //adding the modal trigger listener to the nav button
+        let modal = document.querySelectorAll('.modal')
+        M.Modal.init(modal, { dismissable: true });
+
     },
 }
 document.addEventListener('DOMContentLoaded', APP.init)
